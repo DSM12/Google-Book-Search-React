9cdde03512fc145b3131cd4ac13b7c560c048494
Danny Montano <dsmontano12@gmail.com>
2020-09-23 12:18:49 -0400

    components created, some updated.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SearchContainer from "./components/SearchContainer";
+import SavedContainer from "./components/SavedContainer";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Route exact path="/" component={SearchContainer} />
+      <Route exact path="/saved" component={SavedContainer} />
+    </Router>
+    
+  )
 }
 
 export default App;
